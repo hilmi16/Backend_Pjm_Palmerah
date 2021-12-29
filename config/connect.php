33a5e7572dd13c23623147a4523815c75d7ab23e
hilmi16 <57147552+hilmi16@users.noreply.github.com>
2021-12-29 23:20:44 +0700

    Delete config directory

--- config/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-//Mengkoneksikan PHP dengan database
-    $con = mysqli_connect("localhost", "id14279816_pjmpalmerah", "^[K5\b<=T=&iT4\m", "id14279816_dbpjmpalmerah" );
-//end of file connect.php
